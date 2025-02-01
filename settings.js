3ff65095f3670226317fac66003aa780d6117380
laylays5f <orangetomboye@gmail.com>
2025-02-01 10:37:00 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -40,7 +40,7 @@ global.autoTyping = false //auto typing
 global.autorecordtype = true //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = true //auto update bio
-global.anti92 = false //auto block +92 
+global.anti92 = false //auto block +234
 global.autoswview = true //auto view status/story
 
 //menu type 
